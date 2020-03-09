f3f67e64944a4a6ce4de5037d3c992c7484f9686
panshangzhu <yipan.wang677090@gmail.com>
2020-03-09 15:03:42 -0400

    Updates

---
+++ precache-manifest.bbac3380c859397d1772004a64d44f36.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c21692b0ab8ee0783543b53be62533cf",
+    "url": "/TodoList-TypeScript-Antd-/index.html"
+  },
+  {
+    "revision": "88ba501cad5a2edcbdf8",
+    "url": "/TodoList-TypeScript-Antd-/static/css/2.1bd2ab2f.chunk.css"
+  },
+  {
+    "revision": "ce92e77affe9edfc559a",
+    "url": "/TodoList-TypeScript-Antd-/static/css/main.91e3b67d.chunk.css"
+  },
+  {
+    "revision": "88ba501cad5a2edcbdf8",
+    "url": "/TodoList-TypeScript-Antd-/static/js/2.8249ac75.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/TodoList-TypeScript-Antd-/static/js/2.8249ac75.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce92e77affe9edfc559a",
+    "url": "/TodoList-TypeScript-Antd-/static/js/main.e26050bc.chunk.js"
+  },
+  {
+    "revision": "5f114ca99f1892177be8",
+    "url": "/TodoList-TypeScript-Antd-/static/js/runtime-main.abbc4490.js"
+  }
+]);
